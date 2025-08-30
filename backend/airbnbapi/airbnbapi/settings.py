a7633f9035b332f30cae94bf53f0494456b64548
rahulkumarparida <rahulkumarparida52@gmail.com>
2025-08-30 20:28:49 +0530

    Backend Deployment in process

--- backend/airbnbapi/airbnbapi/settings.py
+++ backend/airbnbapi/airbnbapi/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-2)a!)xw1bqrt(8nid!)wy@yq@ja6nv7_n+uiu8o6)yiv$!nc(7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1' , "https://airbnbapi-6s4b.onrender.com"]
 
 
 # Set it up before migrations
